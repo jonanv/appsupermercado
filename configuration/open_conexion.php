2abbf84b4d65693101ea356ac07c15abfee0b71a
Johanny Vargas González <Giovanni@MacBook-Pro-de-Giovanni.local>
2017-06-01 22:27:32 -0500

    database

--- configuration/open_conexion.php
+++ configuration/open_conexion.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	#Servidor web Heroku-PostgreSQL
-	$conexion = pg_connect("host=ec2-23-23-227-188.compute-1.amazonaws.com dbname=deug4arntm9gvj user=qryfoiywbmxfit password=1c9fd69fa358afe5d9bcc1d23268297b567fbcaea59945804c1e6a0afcc5e86b");
+	$conexion = pg_connect("host=ec2-54-221-255-153.compute-1.amazonaws.com dbname=da721o5fsp76im user=fzivxgvxuynbkb password=a0fb2a0f94539d33479a03f7ae8d1d5be441256d6bb4a7737e4314f1c55ae070");
 
 	#Servidor local
 	#$conexion = pg_connect("host=localhost dbname=supermercado user=postgres password=admin");
